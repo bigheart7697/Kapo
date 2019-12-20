420c7230d9c32a5ee91d84401d463ec2d1863e35
bigheart7697 <rezabigheart76@gmail.com>
2019-12-20 11:49:14 +0330

    models profile and product added

--- Kapo_Back/Kapo_Back/kapo/models.py
+++ Kapo_Back/Kapo_Back/kapo/models.py
@@ -7,6 +7,7 @@ from phone_field import PhoneField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='users/%s/% Y/% m/% d/' % user.id, height_field=None, width_field=None)
     bio = models.TextField(max_length=500, blank=True)
     phone_number = PhoneField(help_text='User phone number')
     city = models.CharField(max_length=100, default='')
@@ -23,14 +24,16 @@ def save_user_profile(sender, instance, **kwargs):
 
 
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     created_date = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, default='')
-    image = models.ImageField(upload_to='products/% Y/% m/% d/', height_field=None, width_field=None)
+    image = models.ImageField(upload_to='products/%s/% Y/% m/% d/' % id, height_field=None, width_field=None)
     description = models.TextField()
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE())
     price = models.IntegerField()
     second_hand = models.BooleanField()
     available = models.BooleanField()
+
 
     class Meta:
         ordering = ['created_date']
