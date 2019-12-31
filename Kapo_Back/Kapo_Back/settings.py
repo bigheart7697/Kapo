20fcad93cf771d19f5817a3cdd5c0a8bd7d9da51
Reza Shirkavand <rezashirkavand1997@gmail.com>
2019-12-31 18:50:55 +0000

    Update settings.py

--- Kapo_Back/Kapo_Back/settings.py
+++ Kapo_Back/Kapo_Back/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from django.utils.translation import ugettext_lazy as _
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,4 @@ LANGUAGES = [
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
