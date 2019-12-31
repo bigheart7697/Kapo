ca4ee82e898a5abe2abb23a15527fc66ad18c3a1
Reza Shirkavand <rezashirkavand1997@gmail.com>
2019-12-31 18:54:55 +0000

    Update settings.py

--- Kapo_Back/Kapo_Back/settings.py
+++ Kapo_Back/Kapo_Back/settings.py
@@ -141,4 +141,4 @@ LANGUAGES = [
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
