a8cf5b8392c1f81a7e9069f130a19356fa46ab41
pouyan shirzadian <pshirzadian26@gmail.com>
2020-01-01 12:04:52 +0330

    tests finished

--- Kapo_Front/kapo/src/components/product-details/index.js
+++ Kapo_Front/kapo/src/components/product-details/index.js
@@ -12,65 +12,65 @@ class ProductDetails extends React.Component {
       <>
         <div className="productDetails__container">
           <div className="productDetails__leftPanel">
-            <h1>{this.props.product.name}</h1>
+            <h1>{this.props.product.name ? this.props.product.name : '-'}</h1>
 
-            <h4 className="ui horizontal divider header">
+            <div className="ui horizontal divider header">
               <i className="tags icon"></i>
               <h4 className="productDetails__h1--no-margin">توضیحات کالا</h4>
-            </h4>
-            <p>{this.props.product.details}</p>
+            </div>
+            <p>{this.props.product.description ? this.props.product.description : '-'}</p>
 
-            <h4 className="ui horizontal divider header">
+            <div className="ui horizontal divider header">
               <i className="bar chart icon"></i>
               <h4 className="productDetails__h1--no-margin"> مشخصات کالا</h4>
-            </h4>
+            </div>
             <table className="ui definition table productDetails__table">
               <tbody>
                 <tr>
                   <td className="productDetails__column">
-                    {this.props.product.second_hand}
+                    {this.props.product.second_hand ? 'دست دوم' : 'نو'}
                   </td>
                   <td className="productDetails__column">نوع کالا</td>
                 </tr>
                 <tr>
-                  <td>{this.props.product.type}</td>
+                  <td>{this.props.product.type ? this.props.product.type : '-'}</td>
                   <td>دسته کالا</td>
                 </tr>
                 <tr>
-                  <td>{this.props.product.availability}</td>
+                  <td>{this.props.product.availability ? this.props.product.availability : '-'}</td>
                   <td>در دسترس بودن</td>
                 </tr>
                 <tr>
-                  <td>{this.props.product.year_produced}</td>
+                  <td>{this.props.product.production_year ? this.props.product.production_year : '-'}</td>
                   <td>سال ساخت</td>
                 </tr>
                 <tr>
-                  <td>{this.props.product.user.type}</td>
+                  <td>{this.props.product.user ? (this.props.product.user.type ? this.props.product.user.type : '-') : '-'}</td>
                   <td>نوع آگهی</td>
                 </tr>
                 <tr>
-                  <td>{this.props.product.price}</td>
+                  <td>{this.props.product.price ? this.props.product.price : '-'}</td>
                   <td>قیمت (تومان)</td>
                 </tr>
               </tbody>
             </table>
 
-            <h4 className="ui horizontal divider header">
+            <div className="ui horizontal divider header">
               <i className="address card outline icon"></i>
               <h4 className="productDetails__h1--no-margin">
                 مشخصات تماس فروشنده
               </h4>
-            </h4>
+            </div>
             <table className="ui definition table productDetails__table">
               <tbody>
                 <tr>
                   <td className="productDetails__column">
-                    {this.props.product.user.name}
+                    {this.props.user ? this.props.product.user.name ? this.props.product.user.name : '-' : '-'}
                   </td>
                   <td className="productDetails__column">نام</td>
                 </tr>
                 <tr>
-                  <td>{this.props.product.user.address}</td>
+                  <td>{this.props.user ? this.props.product.user.address ? this.props.product.user.address : '-' : '-'}</td>
                   <td>آدرس</td>
                 </tr>
               </tbody>
