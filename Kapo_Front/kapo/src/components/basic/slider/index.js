b338333956fcb38f9e9f54904e31ae87030ac26b
Allarious <alirezaicq4@gmail.com>
2020-01-02 17:55:14 +0330

    working on slider

---
+++ Kapo_Front/kapo/src/components/basic/slider/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.scss'
+
+class Slider extends React.Component{
+    render(){
+        return(<div>slider</div>)
+    }
+}
+
+export default Slider;
--- Kapo_Front/kapo/src/components/mainpage/index.js
+++ Kapo_Front/kapo/src/components/mainpage/index.js
@@ -3,10 +3,12 @@ import React from 'react'
 import './style.scss'
 
 import DogAnimation from '../basic/dogAnimation'
+import Slider from '../basic/slider'
 
 const mainpage = () => {
     return(
         <div className="mainpage__container">
+            <Slider/>
             <DogAnimation/>
 		</div>
     )
