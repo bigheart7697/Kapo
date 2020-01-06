2070371b9bf5a5ee46f4863ef09c08ec7642e431
Allarious <alirezaicq4@gmail.com>
2020-01-06 13:42:05 +0330

    added slider

---
+++ Kapo_Front/kapo/src/components/basic/productCard/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.scss'
+
+const productCard = (props) => {
+    return(<div className="product-card__container">
+        <div className="product-card__content"></div>
+    </div>)
+}
+
+export default productCard
--- Kapo_Front/kapo/src/components/mainpage/index.js
+++ Kapo_Front/kapo/src/components/mainpage/index.js
@@ -4,11 +4,42 @@ import './style.scss'
 
 import DogAnimation from '../basic/dogAnimation'
 import Slider from '../basic/slider'
+import ProductCard from '../basic/productCard'
+
+const slideData = [
+    {
+        index: 0,
+        headline: 'New Fashion Apparel',
+        button: 'Shop now',
+        src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/fashion.jpg'
+    },
+    {
+        index: 1,
+        headline: 'In The Wilderness',
+        button: 'Book travel',
+        src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/forest.jpg'
+    },
+    {
+        index: 2,
+        headline: 'For Your Current Mood',
+        button: 'Listen',
+        src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/guitar.jpg'
+    },
+    {
+        index: 3,
+        headline: 'Focus On The Writing',
+        button: 'Get Focused',
+        src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/typewriter.jpg'
+    }
+]
 
 const mainpage = () => {
     return(
         <div className="mainpage__container">
-            <Slider/>
+            <div className="main-page__slider">
+                <Slider heading="Example Slider" slides={slideData} /> 
+            </div>
+            <ProductCard/>
             <DogAnimation/>
 		</div>
     )
