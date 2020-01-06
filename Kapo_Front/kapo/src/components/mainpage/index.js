ebcd5a212ffbf88672e0b742645ded4fc8f9542f
Allarious <alirezaicq4@gmail.com>
2020-01-06 13:49:59 +0330

    fix slider

--- Kapo_Front/kapo/src/components/mainpage/index.js
+++ Kapo_Front/kapo/src/components/mainpage/index.js
@@ -9,26 +9,26 @@ import ProductCard from '../basic/productCard'
 const slideData = [
     {
         index: 0,
-        headline: 'New Fashion Apparel',
-        button: 'Shop now',
+        headline: 'لباس جدید خودت رو انتخاب کن',
+        button: 'همین الآن بخر',
         src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/fashion.jpg'
     },
     {
         index: 1,
-        headline: 'In The Wilderness',
-        button: 'Book travel',
+        headline: 'در دل طبیعت',
+        button: 'کتاب خوانی در میان طبیعت',
         src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/forest.jpg'
     },
     {
         index: 2,
-        headline: 'For Your Current Mood',
-        button: 'Listen',
+        headline: 'حس چه کاریو داری؟',
+        button: 'به آهنگ‌ها گوش کن',
         src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/guitar.jpg'
     },
     {
         index: 3,
-        headline: 'Focus On The Writing',
-        button: 'Get Focused',
+        headline: 'بر روی نوشتن تمرکز کن',
+        button: 'اینجا رو ببین',
         src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/typewriter.jpg'
     }
 ]
