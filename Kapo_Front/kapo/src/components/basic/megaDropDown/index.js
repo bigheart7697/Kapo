247efcf99db1fafacdcf241b86e27bb3dcb89d4d
pouyan shirzadian <pshirzadian26@gmail.com>
2020-01-24 17:55:09 +0330

    mega drop down imporved

--- Kapo_Front/kapo/src/components/basic/megaDropDown/index.js
+++ Kapo_Front/kapo/src/components/basic/megaDropDown/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 
 import './style.scss';
 
+import ItemsColumn from '../itemsColumn';
+
 class MegaDropDown extends React.Component {
-    state = {categories: [], active: false}
+    state = {name: '', categories: [], active: false, image: null}
 
     changeCategories = (props) => {
-        this.setState({categories: props.categories})
+        this.setState({categories: props.categories, image: props.image, name: props.name})
         this.setState({active: true})
+        this.refs.itemscolumn.changeCategories([])
     }
 
     changeDeActive = () => {
         this.setState({active: false})
+        this.refs.itemscolumn.changeCategories([])
+    }
+
+    changeSubCategories = (categories) => {
+        this.refs.itemscolumn.changeCategories(categories)
     }
 
     render() {
@@ -19,16 +27,19 @@ class MegaDropDown extends React.Component {
             <div
                 className={this.state.active ? 'mega-drop-down__container mega-drop-down__container--active' : 'mega-drop-down__container mega-drop-down__container--hide'}>
                 <div className={this.state.active ? 'mega-drop-down__content mega-drop-down__content--active' : 'mega-drop-down__content mega-drop-down__content--hide'}>
-                    {this.state.categories.map((element, index) => {
-                        return (
-                            <div key={2*index} className="mega-drop-down__column">
-                                <div key={1+2*index} className="mega-drop-down__column-header">{element.name}</div>
-                                {element.categories.map((e, i) => {
-                                    return <div key={-1-i} className="mega-drop-down__column-items">{e.name}</div>
-                                })}
-                            </div>
-                        );
-                    })}
+                    <div className="mega-drop-down__column mega-drop-down__column--background-hover">
+                        {this.state.categories.map((element, index) => {
+                            return (
+                                <div key={1+2*index} className="mega-drop-down__column-header" 
+                                    onMouseOver={() => this.changeSubCategories(element.categories)} 
+                                    onMouseLeave={() => this.changeSubCategories(element.categories)}>{element.name}</div>
+                            );
+                        })}
+                    </div>
+                    <ItemsColumn ref='itemscolumn' categories={this.state.categories ? this.state.categories[0] ? this.state.categories[0].categories : [] : []}/>
+                    <div className='mega-drop-down__column'>
+                        <img src={this.state.image} alt={this.state.name} className='mega-drop-down__image'></img>
+                    </div>
                 </div>
             </div>
         );
--- Kapo_Front/kapo/src/components/nav-bar/index.js
+++ Kapo_Front/kapo/src/components/nav-bar/index.js
@@ -6,31 +6,36 @@ import SearchBar from '../basic/searchBar';
 import MegaDropDown from '../basic/megaDropDown';
 
 import logo from '../../assets/Logo1.png';
+import image1 from '../../assets/1.png';
+import image2 from '../../assets/2.png';
+import image3 from '../../assets/3.png';
+import image4 from '../../assets/4.png';
+import image5 from '../../assets/5.png';
 
 class Navbar extends React.Component {
   state = { active: 10, dropActive: false, categories: [] };
 
   componentDidMount() {
     this.setState({categories : [
-      {name: 'املاک', categories: [
+      {name: 'املاک', image: image1, categories: [
         {name: 'رهن', categories: [{name: 'نیاوران'}, {name: 'پاسداران'}]},
         {name: 'اجاره', categories: [{name: 'دو خوابه'}, {name: 'سه خوابه'}, {name: 'بیشتر'}]}
       ]},
-      {name: 'ماشین', categories: [
+      {name: 'ماشین', image: image2, categories: [
         {name: 'لوکس', categories: [{name: 'بنتلی'}, {name: 'بنز'}]},
         {name: 'اسپورت', categories: [{name: 'لامبورگینی'}, {name: 'بوگاتی'}]},
         {name: 'ایرانی', categories: [{name: 'پراید'}, {name: 'پیکان'}]}
       ]},
-      {name: 'کالای برقی', categories: [
+      {name: 'کالای برقی', image: image3, categories: [
         {name: 'خانگی', categories: [{name: 'جاروبرقی'}, {name: 'ماکروفر'}]},
         {name: 'لوازم جانبی', categories: [{name: 'شارژر'}]}
       ]},
-      {name: 'لباس', categories: [
+      {name: 'لباس', image: image4, categories: [
         {name: 'زنانه', categories: [{name: 'کفش'}, {name: 'شلوار'}]},
         {name: 'مردانه', categories: [{name: 'شلوار'}, {name: 'پیراهن'}]},
         {name: 'بچگانه', categories: [{name: 'پسرانه'}, {name: 'دخترانه'}]}
       ]},
-      {name: 'موسیقی', categories: []}
+      {name: 'موسیقی', image: image5, categories: []}
     ]})
   }
 
@@ -58,7 +63,7 @@ class Navbar extends React.Component {
             </div>
             <div className='nav-bar__row nav-bar__row--short'>
               {this.state.categories.map((element, index) => {
-                return <div className='nav-bar__category' key={index} onMouseOver={() => this.refs.mega.changeCategories({categories: element.categories})} onMouseLeave={() => this.refs.mega.changeDeActive()}>{element.name}</div>;
+                return <div className='nav-bar__category' key={index} onMouseOver={() => this.refs.mega.changeCategories({categories: element.categories, image: element.image, name: element.name})} onMouseLeave={() => this.refs.mega.changeDeActive()}>{element.name}</div>;
               })}
             </div>
           </div>
