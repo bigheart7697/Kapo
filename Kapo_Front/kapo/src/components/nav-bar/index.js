7f041d95b679d34d2860881c39110dbcb6f7d8e3
pouyan shirzadian <pshirzadian26@gmail.com>
2020-01-24 11:57:55 +0330

    nav-bar component improved

--- Kapo_Front/kapo/src/components/nav-bar/index.js
+++ Kapo_Front/kapo/src/components/nav-bar/index.js
@@ -10,53 +10,54 @@ import SearchBar from '../basic/searchBar';
 
 import logo from '../../assets/Logo1.png';
 
-const navItems = [
-  {
-    text: "صفحه اصلی",
-    link: "/"
-  },
-  {
-    text: "اضافه کردن محصول",
-    link: "/addProduct"
-  },
-  {
-    text: "محصولات",
-    link: "/ProductList"
-  },
-  {
-    text: "ورود",
-    link: "/Auth/SignIn"
-  },
-  {
-    text: "ثبت نام",
-    link: "/Auth/SignUp"
-  },
-  {
-    text: "خروج",
-    link: "/"
-  }
-];
-
 class Navbar extends React.Component {
-  state = { active: 0, dropActive: false };
-  navItemOnClick = (index, link) => {
-    this.setState({ active : index })
-    history.push(link)
+  state = { active: 10, dropActive: false, categories: [] };
+
+  componentDidMount() {
+    this.setState({categories : [
+      {name: 'املاک', categories: []},
+      {name: 'ماشین', categories: []},
+      {name: 'کالای برقی', categories: []},
+      {name: 'لباس', categories: []},
+      {name: 'موسیقی', categories: []}
+    ]})
   }
+
+
   render() {
     return (
       <div className='nav-bar__container'>
-        <div className='nav-bar__row nav-bar__row--tall'>
-          <div className='nav-bar__item nav-bar__item--image'>
-            <img src={logo} className='nav-bar__logo'/>
-          </div>
-          <div className='nav-bar__item nav-bar__item--display-table nav-bar__item--long'>
-            <SearchBar />
-          </div>
-          <div className='nav-bar__item'>
-          </div>
+        <div className='nav-bar__image'>
+          <img src={logo} className='nav-bar__logo'/>
         </div>
-        <div className='nav-bar__row nav-bar__row--short'>
+        <div className='nav-bar__content'>
+          <div className='nav-bar__row nav-bar__row--tall'>
+            
+            <div className='nav-bar__item nav-bar__item--display-table nav-bar__item--long'>
+              <SearchBar />
+            </div>
+            <div className='nav-bar__item nav-bar__item--items'>
+              <div className='nav-bar__button nav-bar__button--red'>
+                ثبت محصول جدید
+              </div>
+              <div className='nav-bar__button'>
+                ثبت‌نام / ورود
+              </div>
+            </div>
+          </div>
+          <div className='nav-bar__row nav-bar__row--short'>
+            {this.state.categories.map((element, index) => {
+              if (this.state.active === index) {
+                return (
+                  <NavItem key={index} active>
+                    {element.text}
+                  </NavItem>
+                );
+              } else {
+                return <div className='nav-bar__category' key={index}>{element.name}</div>;
+              }
+            })}
+          </div>
         </div>
       </div>
     );
