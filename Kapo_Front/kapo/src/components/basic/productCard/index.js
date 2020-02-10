3d0358ecce816f2f6e195483cd8fe16766b3bab1
bigheart7697 <rezabigheart76@gmail.com>
2020-02-10 14:22:23 +0330

    search problem solved

--- Kapo_Front/kapo/src/components/basic/productCard/index.js
+++ Kapo_Front/kapo/src/components/basic/productCard/index.js
@@ -6,22 +6,22 @@ import './style.scss'
 class productCard extends React.Component {
     state={mouseOver: false, element: this.props.product}
     render() {
-        console.log(this.state.element.image);
+        console.log(this.state.element.name);
         console.log(`url(${defaultImg})`);
         console.log(`url(${this.state.element.image})`);
         
         
-        return (<div className="product-card__container" onClick={this.props.onClick} onMouseEnter={() => this.setState({ mouseOver: true})} onMouseLeave={() => this.setState({ mouseOver: false })} style={{ backgroundImage: this.state.element.image? `url(${this.state.element.image})` : `url(${defaultImg})` }}>
+        return (<div className="product-card__container" onClick={this.props.onClick} onMouseEnter={() => this.setState({ mouseOver: true})} onMouseLeave={() => this.setState({ mouseOver: false })} style={{ backgroundImage: this.props.product.image? `url(${this.props.product.image})` : `url(${defaultImg})` }}>
             <div className="product-card__content">
-                <div className={!this.state.mouseOver ? "product-card__name" : "product-card__name product-card__name--hover"}>{this.state.element.name}</div>
+                <div className={!this.state.mouseOver ? "product-card__name" : "product-card__name product-card__name--hover"}>{this.props.product.name}</div>
                 {!this.state.mouseOver 
-                ? <div className="product-card__description">{this.state.element.description}</div>
+                ? <div className="product-card__description">{this.props.product.description}</div>
                 : (<div className="product-card__details-button-container">
                     <div className="product-card__details-button">مشاهده جزییات</div>
                 </div>)}
                 <div className="product-card__image-container">
                     <div className="product-card__image-before" />
-                    <div className="product-card__image" style={{ backgroundImage: this.state.element.image? `url(${this.state.element.image})` : `url(${defaultImg})` }}></div>
+                    <div className="product-card__image" style={{ backgroundImage: this.props.product.image? `url(${this.props.product.image})` : `url(${defaultImg})` }}></div>
                 </div>
             </div>
         </div>)
--- Kapo_Front/kapo/src/components/product-list/index.js
+++ Kapo_Front/kapo/src/components/product-list/index.js
@@ -7,56 +7,10 @@ import _ from "lodash";
 
 import './style.scss'
 
-import SearchBar from '../../components/basic/searchBar'
 import ProductCard from '../basic/productCard'
 import CustomSelect from '../basic/customSelect'
 
-import image1 from '../../assets/1.png'
-import image2 from '../../assets/2.png'
-import image3 from '../../assets/3.png'
-import image4 from '../../assets/4.png'
-import image5 from '../../assets/5.png'
-import image6 from '../../assets/6.png'
 
-const PRODUCT_LIST = [
-    {
-        image: image6,
-        name: 'تست',
-        description: 'خیلی خوبه',
-        price: '12000',
-        owner: {'corporate_name': 'شرکت علی بابا', 'address': 'نیاوران', 'country': 'ایران', 'city': 'تهران', 'corporate_number': '123456789101', 'corporate_economic_number': '432198765', 'phone_number': '02122222222'}
-    }, {
-        image: image5,
-        name: 'تست',
-        description: 'خیلی خوبه',
-        price: '12000',
-        owner: {'corporate_name': 'شرکت علی بابا', 'address': 'نیاوران', 'country': 'ایران', 'city': 'تهران', 'corporate_number': '123456789101', 'corporate_economic_number': '432198765', 'phone_number': '02122222222'}
-    }, {
-        image: image4,
-        name: 'تست',
-        description: 'خیلی خوبه',
-        price: '12000',
-        owner: {'corporate_name': 'شرکت علی بابا', 'address': 'نیاوران', 'country': 'ایران', 'city': 'تهران', 'corporate_number': '123456789101', 'corporate_economic_number': '432198765', 'phone_number': '02122222222'}
-    }, {
-        image: image3,
-        name: 'تست',
-        description: 'خیلی خوبه',
-        price: '12000',
-        owner: {'corporate_name': 'شرکت علی بابا', 'address': 'نیاوران', 'country': 'ایران', 'city': 'تهران', 'corporate_number': '123456789101', 'corporate_economic_number': '432198765', 'phone_number': '02122222222'}
-    }, {
-        image: image2,
-        name: 'تست',
-        description: 'خیلی خوبه',
-        price: '12000',
-        owner: {'corporate_name': 'شرکت علی بابا', 'address': 'نیاوران', 'country': 'ایران', 'city': 'تهران', 'corporate_number': '123456789101', 'corporate_economic_number': '432198765', 'phone_number': '02122222222'}
-    }, {
-        image: image1,
-        name: 'تست',
-        description: 'خیلی خوبه',
-        price: '12000',
-        owner: {'corporate_name': 'شرکت علی بابا', 'address': 'نیاوران', 'country': 'ایران', 'city': 'تهران', 'corporate_number': '123456789101', 'corporate_economic_number': '432198765', 'phone_number': '02122222222'}
-    }
-]
 
 class Productlist extends React.Component {
     state = {query: "", category: 0}
@@ -78,7 +32,6 @@ class Productlist extends React.Component {
         console.log(this.props.newArray);
         
         return (<>
-            {/* <SearchBar onSearch={this.onSearch} /> */}
             <div className="product-list__search">
                 <CustomSelect content={array} label="دسته‌بندی‌ها" input={{onChange: () => this.setState({category:0})}}/>
             </div>
