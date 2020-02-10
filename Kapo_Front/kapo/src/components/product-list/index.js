3163f0dd0afcd1fed5ef279a5d139737e516249d
pouyan shirzadian <pshirzadian26@gmail.com>
2020-02-10 15:26:11 +0330

    sponsered product cards

--- Kapo_Front/kapo/src/components/product-list/index.js
+++ Kapo_Front/kapo/src/components/product-list/index.js
@@ -21,36 +21,42 @@ import image6 from '../../assets/6.png'
 const PRODUCT_LIST = [
     {
         image: image6,
+        is_sponsered: true,
         name: 'تست',
         description: 'خیلی خوبه',
         price: '12000',
         owner: {'corporate_name': 'شرکت علی بابا', 'address': 'نیاوران', 'country': 'ایران', 'city': 'تهران', 'corporate_number': '123456789101', 'corporate_economic_number': '432198765', 'phone_number': '02122222222'}
     }, {
         image: image5,
+        is_sponsered: true,
         name: 'تست',
         description: 'خیلی خوبه',
         price: '12000',
         owner: {'corporate_name': 'شرکت علی بابا', 'address': 'نیاوران', 'country': 'ایران', 'city': 'تهران', 'corporate_number': '123456789101', 'corporate_economic_number': '432198765', 'phone_number': '02122222222'}
     }, {
         image: image4,
+        is_sponsered: false,
         name: 'تست',
         description: 'خیلی خوبه',
         price: '12000',
         owner: {'corporate_name': 'شرکت علی بابا', 'address': 'نیاوران', 'country': 'ایران', 'city': 'تهران', 'corporate_number': '123456789101', 'corporate_economic_number': '432198765', 'phone_number': '02122222222'}
     }, {
         image: image3,
+        is_sponsered: false,
         name: 'تست',
         description: 'خیلی خوبه',
         price: '12000',
         owner: {'corporate_name': 'شرکت علی بابا', 'address': 'نیاوران', 'country': 'ایران', 'city': 'تهران', 'corporate_number': '123456789101', 'corporate_economic_number': '432198765', 'phone_number': '02122222222'}
     }, {
         image: image2,
+        is_sponsered: false,
         name: 'تست',
         description: 'خیلی خوبه',
         price: '12000',
         owner: {'corporate_name': 'شرکت علی بابا', 'address': 'نیاوران', 'country': 'ایران', 'city': 'تهران', 'corporate_number': '123456789101', 'corporate_economic_number': '432198765', 'phone_number': '02122222222'}
     }, {
         image: image1,
+        is_sponsered: false,
         name: 'تست',
         description: 'خیلی خوبه',
         price: '12000',
@@ -83,7 +89,7 @@ class Productlist extends React.Component {
                 <CustomSelect content={array} label="دسته‌بندی‌ها" input={{onChange: () => this.setState({category:0})}}/>
             </div>
             <div className="product-list__container">
-                {this.props.newArray.map((element, index) => <ProductCard key={index} onClick={() => history.push(`/product/${element.id}`)} product={element}></ProductCard>)}
+                {PRODUCT_LIST.map((element, index) => <ProductCard key={index} onClick={() => history.push(`/product/${element.id}`)} product={element} is_sponsered={element.is_sponsered}></ProductCard>)}
             </div>
         </>)
     }
