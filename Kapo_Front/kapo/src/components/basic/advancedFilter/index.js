311b3493bb747636329223855e42d9096a06a273
Allarious <alirezaicq4@gmail.com>
2020-02-11 10:33:40 +0330

    adding advanced filters

---
+++ Kapo_Front/kapo/src/components/basic/advancedFilter/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './style.scss'
+
+class AdvancedFilter extends React.Component{
+    render(){
+        return(<div className="advanced-filter__container">
+            something
+        </div>)
+    }
+}
+
+export default AdvancedFilter
