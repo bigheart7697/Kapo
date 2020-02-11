5e4416fa52654c1cabd174b741704dba5875ef17
pouyan shirzadian <pshirzadian26@gmail.com>
2020-02-11 21:21:09 +0330

    set price fixed

--- Kapo_Front/kapo/src/components/AddProduct/index.js
+++ Kapo_Front/kapo/src/components/AddProduct/index.js
@@ -6,6 +6,7 @@ import "./style.scss";
 
 import Form from '../form'
 import FormWrapper from '../formWrapper'
+import SetPrice from '../setPrice'
 
 const FORM_VALUES = {
   submitText: "ثبت",
@@ -55,36 +56,20 @@ const FORM_VALUES = {
       label: "تصویر کالا",
       type: "file",
       error: "لطفا تصویر کالا را بارگذاری کنید"
+    },
+    {
+      title: "price",
+      label: "قیمت",
+      inputType: "number",
+      error: "لطفا قیمت کالای موجود را وارد کنید"
     }
   ]
 };
 
-const categories = [
-  {text: 'املاک', value: '1', categories: [
-    {text: 'رهن', value: '1', categories: [{text: 'نیاوران', value: '1'}, {text: 'پاسداران', value: '2'}]},
-    {text: 'اجاره', value: '2', categories: [{text: 'دو خوابه', value: '1'}, {text: 'سه خوابه', value: '2'}, {text: 'بیشتر', value: '3'}]}
-  ]},
-  {text: 'ماشین', value: '2', categories: [
-    {text: 'لوکس', value: '1', categories: [{text: 'بنتلی', value: '1'}, {text: 'بنز', value: '2'}]},
-    {text: 'اسپورت', value: '2', categories: [{text: 'لامبورگینی', value: '1'}, {text: 'بوگاتی', value: '2'}]},
-    {text: 'ایرانی', value: '3', categories: [{text: 'پراید', value: '1'}, {text: 'پیکان', value: '2'}]}
-  ]},
-  {text: 'کالای برقی', value: '3', categories: [
-    {text: 'خانگی', value: '1', categories: [{text: 'جاروبرقی', value: '1'}, {text: 'ماکروفر', value: '2'}]},
-    {text: 'لوازم جانبی', value: '2', categories: [{text: 'شارژر', value: '1'}]}
-  ]},
-  {text: 'لباس', value: '4', categories: [
-    {text: 'زنانه', value: '1', categories: [{text: 'کفش', value: '1'}, {text: 'شلوار', value: '2'}]},
-    {text: 'مردانه', value: '2', categories: [{text: 'شلوار', value: '1'}, {text: 'پیراهن', value: '2'}]},
-    {text: 'بچگانه', value: '3', categories: [{text: 'پسرانه', value: '1'}, {text: 'دخترانه', value: '2'}]}
-  ]},
-  {text: 'موسیقی', value: '5', categories: []}
-];
-
 class AddProduct extends React.Component {
   constructor(props) {
     super(props) 
-    this.state = {categories1: categories, categories2: [], categories3: []}
+    this.state = {categories1: [], categories2: [], categories3: []}
   }
 
   componentDidMount() {
@@ -108,19 +93,22 @@ class AddProduct extends React.Component {
 
   render() {
     return (
-      <FormWrapper>
-        <Form
-          formValues={FORM_VALUES.form_inputs}
-          onSubmit={this.onSubmit}
-          submitText={FORM_VALUES.submitText}
-          title={FORM_VALUES.title}
-          categories1={this.props.category_hierarchy? this.props.category_hierarchy.categories : this.state.categories1}
-          categories2={this.state.categories2}
-          categories3={this.state.categories3}
-          onChangeCategory1={this.onChangeCategory1}
-          onChangeCategory2={this.onChangeCategory2}
-        ></Form>
-      </FormWrapper>
+      <>
+        <FormWrapper>
+          <Form
+            formValues={FORM_VALUES.form_inputs}
+            onSubmit={this.onSubmit}
+            submitText={FORM_VALUES.submitText}
+            title={FORM_VALUES.title}
+            categories1={this.props.category_hierarchy? this.props.category_hierarchy.categories : this.state.categories1}
+            categories2={this.state.categories2}
+            categories3={this.state.categories3}
+            onChangeCategory1={this.onChangeCategory1}
+            onChangeCategory2={this.onChangeCategory2}
+          ></Form>
+        </FormWrapper>
+        <SetPrice />
+      </>
     );
   }
 }
--- Kapo_Front/kapo/src/components/setPrice/index.js
+++ Kapo_Front/kapo/src/components/setPrice/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import './style.scss'
 import { fetchProducts, setPrice } from '../../actions'
-import Form from '../form'
 import Product from '../product'
 import { connect } from 'react-redux'
 import _ from "lodash";
@@ -12,19 +11,6 @@ import image3 from '../../assets/3.png'
 import image4 from '../../assets/4.png'
 import image5 from '../../assets/5.png'
 import image6 from '../../assets/6.png'
-
-const FORM_VALUES = {
-  submitText: "ثبت",
-  title: "ثبت قیمت کالا",
-  form_inputs: [
-    {
-      title: "price",
-      label: "قیمت",
-      inputType: "number",
-      error: "لطفا قیمت کالای موجود را وارد کنید"
-    }
-  ]
-};
 
 class SetPrice extends React.Component {
     state = {products: []}
@@ -67,16 +53,6 @@ class SetPrice extends React.Component {
                                 return <Product product={element} key={-2-index}></Product>
                             }) : <div></div>}
                         </div>
-                    </div>
-                </div>
-                <div className='set-price__section'>
-                    <div className='set-price__form-wrapper'>
-                        <Form
-                            formValues={FORM_VALUES.form_inputs}
-                            onSubmit={this.onSubmit}
-                            submitText={FORM_VALUES.submitText}
-                            title={FORM_VALUES.title}
-                        ></Form>
                     </div>
                 </div>
             </div>
