54d50ae9b315532810143bfefc1e5969ab09e899
Allarious <alirezaicq4@gmail.com>
2020-02-12 21:57:31 +0330

    added functionality to advanced filter

--- Kapo_Front/kapo/src/components/basic/advancedFilter/index.js
+++ Kapo_Front/kapo/src/components/basic/advancedFilter/index.js
@@ -10,57 +10,58 @@ import CustomReactSwitch from '../customReactSwitch'
 import CustomSelect from '../customSelect'
 import AdvancedSorting from '../advancedSorting'
 
-const sth = [{text: 'sth', label: 'sth new'}, {text: 'sth1', label: 'sth1 new'}, {text: 'sth2', label: 'sth2 new'}]
-
 class AdvancedFilter extends React.Component{
-    state = {cat1: null, cat2: null, cat3: null}
+    state = {priceRange: null, productionYearRange: null, quantityRange: null, secondHandOnly: false, cat1: null, cat2: null, cat3: null, activeSort: 0, direction: 0}
     componentDidMount(){
         this.props.fetchCategoryHierarchy()
     }
     render(){
+        console.log(this.state)
         let categories1 = [], categories2 = [], categories3 =[]
         if(this.props.categories){
             this.props.categories.forEach((category) => {
-                categories1.push({ text: category.name, label: category.value, categories: category.categories })
+                categories1.push({ text: category.name, label: category.text ,value: category.value, categories: category.categories })
             })
-        }
-        if(this.props.categories1){
-            categories1.categories.forEach((category) => {
-               categories2.push({ text: category.name, label: category.value, categories: category.categories }) 
-            })
-        }
-        if(this.props.categories2){
-            categories2.categories.forEach((category) => {
-               categories3.push({ text: category.name, label: category.value, categories: category.categories }) 
-            })
+            let searchedValue = searchValue(this.state.cat1, categories1)
+            if(searchedValue){
+                searchedValue.categories.forEach((category) => {
+                    categories2.push({ text: category.name, label: category.text, value: category.value, categories: category.categories }) 
+                })
+            }
+            searchedValue = searchValue(this.state.cat2, categories2)
+            if(searchedValue){
+                searchedValue.categories.forEach((category) => {
+                    categories3.push({ text: category.name, label: category.text, value: category.value, categories: category.categories }) 
+                })
+            }
         }
         return(<div className="advanced-filter__container">
             <div className="advanced-filter__content">
                 <div className="advanced-filter__header">فیلتر</div>
                 <div className="advanced-filter__field">
-                    <CustomRangeSlider min={0} max={100} header="بازه قیمت"/>
+                    <CustomRangeSlider min={0} max={100} header="بازه قیمت" syncState={(range) => { if(this.state.priceRange != range){this.setState({ priceRange: range })} }}/>
                 </div>
                 <div className="advanced-filter__field">
-                    <CustomRangeSlider min={0} max={100} header="سال تولید"/>
+                    <CustomRangeSlider min={0} max={100} header="سال تولید" syncState={(range) => { if(this.state.productionYearRange != range){this.setState({ productionYearRange: range })} }}/>
                 </div>
                 <div className="advanced-filter__field">
-                    <CustomRangeSlider min={0} max={100} header="تعداد موجود"/>
+                    <CustomRangeSlider min={0} max={100} header="تعداد موجود" syncState={(range) => { if(this.state.quantityRange != range){this.setState({ quantityRange: range })} }}/>
                 </div>
                 <div className="advanced-filter__field">
-                    <CustomReactSwitch label="فقط کالا‌های دسته دوم"/>
+                    <CustomReactSwitch label="فقط کالا‌های دسته دوم" handleChange={(checked) => {this.setState({secondHandOnly: checked})}} checked={this.state.secondHandOnly}/>
                 </div>
                 {/* TODO inputs should be fixed */}
                 <div className="advanced-filter__field">
-                    <CustomSelect name='cat1' label='دسته اول' content={categories1} full normal/>
+                    <CustomSelect name='cat1' label='دسته اول' content={categories1} onChange={(e) => {e.persist(); this.setState({ cat1: e.target.value, cat2: null, cat3: null })}} full normal/>
                 </div>
                 <div>
-                    <CustomSelect name='cat2' label='دسته دوم' content={categories2} full normal/>
+                    <CustomSelect name='cat2' label='دسته دوم' content={categories2} onChange={(e) => {e.persist(); this.setState({ cat2: e.target.value, cat3: null })}} full normal/>
                 </div>
                 <div>
-                    <CustomSelect name='cat3' label='دسته سوم' content={categories3} full normal/>
+                    <CustomSelect name='cat3' label='دسته سوم' content={categories3} onChange={(e) => {e.persist(); this.setState({ cat3: e.target.value })}} full normal/>
                 </div>
                 <div className="advanced-filter__header">مرتب‌سازی</div>
-                <AdvancedSorting/>
+                <AdvancedSorting syncState={(sort, direction) => {if(this.state.activeSort != sort || this.state.direction != direction){ this.setState({ activeSort: sort, direction: direction }) }}}/>
             </div>
         </div>)
     }
@@ -70,6 +71,15 @@ const mapStateToProps = state => {
     return(
         { categories: state.products.category_hierarchy ? state.products.category_hierarchy.categories : null }
     )
+}
+
+const searchValue = (value, myArray) => {
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].value === value) {
+            return myArray[i];
+        }
+    }
+    return null
 }
 
 export default connect(mapStateToProps, { fetchCategoryHierarchy })(AdvancedFilter)
--- Kapo_Front/kapo/src/components/basic/customReactSwitch/index.js
+++ Kapo_Front/kapo/src/components/basic/customReactSwitch/index.js
@@ -3,27 +3,15 @@ import Switch from "react-switch";
 
 import './style.scss'
 
-class SwitchExample extends Component {
-  constructor() {
-    super();
-    this.state = { checked: false };
-    this.handleChange = this.handleChange.bind(this);
-  }
- 
-  handleChange(checked) {
-    this.setState({ checked });
-  }
- 
-  render() {
-    return (
-      <div className="custom-react-switch__container">
-        <span>{this.props.label}</span>
-        <div className="custom-react-switch__switch">
-            <Switch onChange={this.handleChange} checked={this.state.checked} />
-        </div>
+const SwitchExample = props => {
+  return (
+    <div className="custom-react-switch__container">
+      <span>{props.label}</span>
+      <div className="custom-react-switch__switch">
+          <Switch onChange={props.handleChange} checked={props.checked} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default SwitchExample
