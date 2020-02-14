b30e37f1197c5baabd7a1cfd2038f37b80fad4c9
Reza Shirkavand <reza@Rezas-MacBook-Pro-4.local>
2020-02-14 21:34:05 +0330

    update permissions

--- Kapo_Back/adminStatistics/permissions.py
+++ Kapo_Back/adminStatistics/permissions.py
@@ -3,7 +3,7 @@ from django.core.exceptions import PermissionDenied
 
 
 class IsStaff(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+    def has_permission(self, request, view):
         if not request.user.is_staff:
             raise PermissionDenied()
         return True
