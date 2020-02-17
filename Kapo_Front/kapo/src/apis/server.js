5dfbb12b42bb27b14e9c46036691a8a91bcc2b67
Allarious <alirezaicq4@gmail.com>
2020-02-17 04:23:55 +0330

    added 404 page handle

--- Kapo_Front/kapo/src/apis/server.js
+++ Kapo_Front/kapo/src/apis/server.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    // baseURL: 'https://kapokala.herokuapp.com'
-    baseURL: 'http://127.0.0.1:8000'
+    baseURL: 'https://kapokala.herokuapp.com'
 })
